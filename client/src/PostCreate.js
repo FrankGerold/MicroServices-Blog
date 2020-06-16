26f02982ce0097555b134e19747aa82066bf4e76
Frank Gerold <FrankETSGerold@gmail.com>
2020-06-16 19:30:30 -0400

    Post sumbission component works. Added cors to other modules. CHanged dev scripts.

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
-
-import axios from 'axios'
+import axios from 'axios';
 
 export default () => {
   const [title, setTitle] = useState('');
 
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('http://localhost:3001/posts', {
+      title
+    });
+
+    setTitle('')
+  }
+
+
   return <div>
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="form-group">
         <label>Title</label>
-        <input className="form-control" />
+        <input className="form-control" value={title} onChange={e => setTitle(e.target.value)} />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>

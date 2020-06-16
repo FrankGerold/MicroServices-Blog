e310346a9210b59681cf12d467b06863abd06692
Frank Gerold <FrankETSGerold@gmail.com>
2020-06-16 15:47:57 -0400

    Initialize intro to microservices blog app

---
+++ posts/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/posts', (req, res) => {
+
+})
+
+
+app.post('/posts', (req, res) => {
+
+})
+
+
+app.listen(3003, () => {
+  console.log('Listening on Port 3003');
+})

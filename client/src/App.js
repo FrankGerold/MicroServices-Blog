3fd8b2d59f32877b9901b5c839e0fab9995fb6e4
Frank Gerold <FrankETSGerold@gmail.com>
2020-06-16 20:21:57 -0400

    Post list renders in app. cant get cards to work for some reason

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import PostCreate from './PostCreate'
+import PostList from './PostList'
 
 export default () => {
   return <div className="container">
     <h1>Blogging With Microservices</h1>
     <PostCreate />
+    <PostList />
   </div>;
 }

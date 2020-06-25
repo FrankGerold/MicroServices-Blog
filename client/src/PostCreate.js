49cc1fc3e181e3ae5b247437ba3ecb9f1f6aa38d
Frank Gerold <FrankETSGerold@gmail.com>
2020-06-25 17:13:46 -0400

    Add Skaffold config for live editing.

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -19,7 +19,7 @@ export default () => {
   return <div>
     <form onSubmit={onSubmit}>
       <div className="form-group">
-        <label>Title</label>
+        <label>Create Post</label>
         <input className="form-control" value={title} onChange={e => setTitle(e.target.value)} />
       </div>
       <button className="btn btn-primary">Submit</button>
